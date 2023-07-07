c5c5cd9cf56edd1351c834adb8d00de70c5b32fd
João Vitor Santos Raimondi <124799423+JoaoPotencia@users.noreply.github.com>
2023-07-07 14:35:18 -0300

    Update Pessoa.java

--- Atividades/Atv10/notebook/Pessoa.java
+++ Atividades/Atv10/notebook/Pessoa.java
@@ -1,1 +1,34 @@
+public class Pessoa{ // Classe do programa
+    
+  //Declarando variaveis de atributos privados nome, idade e altura
+  private String nome;
+  private int idade;
+  private double altura;
 
+    //metodo public,declarado como getNome, para retornar String do nome do objeto
+  public String getNome(){
+    return nome;
+  }
+  //metodo public, declarado como getIdade, para retornar inteiro da idade do objeto
+  public int getIdade() {
+    return idade;
+  }
+  //metodo public, declarado como getAltura, para retornar double da altura do objeto
+  public double getAltura() {
+    return altura;
+  }
+  
+  //metodo sem retorno, declarado como setNome para setar o nome do tipo String
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+  
+  //metodo sem retorno, declarado como setIdade para setar a idade do tipo inteiro
+  public void setIdade(int idade) {
+    this.idade = idade;
+  }
+  //metodo sem retorno, declarado como setAltura para setar a altura do tipo double
+  public void setAltura(double altura) {
+    this.altura = altura;
+  }
+}
