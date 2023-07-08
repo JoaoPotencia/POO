5d51e42b5f48a0ddb9019bb2afaffde0b26210f8
João Vitor Santos Raimondi <124799423+JoaoPotencia@users.noreply.github.com>
2023-07-07 21:15:39 -0300

    Emprestimo.java

--- Atividades/Atv11/Emprestimo.java
+++ Atividades/Atv11/Emprestimo.java
@@ -5,18 +5,18 @@ public class Emprestimo { // Classe do programa Emprestimo
     public double taxaJuros;
     public int numeroParcelas;
     
-    //variavel que auxilia o numero de parcelas para verificar se existem parcelas disponiveis
+    //Declarando auxNumParecals que auxilia para verificar se existem parcelas disponiveis
     int auxNumParcelas = 0; 
 
-    public double proximaParcela(){ //começo do metodo proximaParcela
-        if ((this.numeroParcelas - this.auxNumParcelas) > 0){ // se ainda existirem parcelas
-            final double juros = valorTotal * taxaJuros; //calculo dos juros de cada parcela
-            double valor = (valorTotal / (double) numeroParcelas) + juros; //calcula o valor da parcela somando os juros
-            auxNumParcelas++; //variavel aux do numero de parcelas recebe mais 1
-            return valor; //retorna o valor da parcela ja com os juros
-        } else { //caso não tenha parcelas ainda
-            return -1.0; //retorna -1 pois nao existem parcelas
-        } //fim das comparações e do else
+    public double proximaParcela(){ //declarando metodo proximaParcela
+        if ((this.numeroParcelas - this.auxNumParcelas) > 0){ //if se ainda existirem parcelas disponiveis
+            final double juros = valorTotal * taxaJuros; //Calculo realizado dos juros de cada parcela
+            double valor = (valorTotal / (double) numeroParcelas) + juros; //Calculo da parcela somando os juros
+            auxNumParcelas++; //variavel auxNumParcelas recebe mais 1
+            return valor; //retorna o valor da parcela ja acresentada com os juros
+        } else { //caso nao tenha parcelas retornar -1
+            return -1.0; 
+        }
     } //fim do metodo proximaParcela
 
-} //fim da classe emprestimo
+} //fim da classe Emprestimo
